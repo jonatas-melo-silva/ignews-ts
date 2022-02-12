9204c79572a41c5e889386f1ffd991f382b9ed65
jonatas-melo-silva <jonatas891024@gmail.com>
2022-02-11 23:53:38 -0300

    ✨Adicionado componente SubscribeButton e estilo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { SubscribeButton } from '../components/SubscribeButton'
 
 import styles from './home.module.scss'
 
@@ -11,11 +12,14 @@ export default function Home() {
       <main className={styles.contentContainer}>
         <section className={styles.hero}>
           <span>👏 Hey, welcome</span>
-          <h1>News about the <span>React</span> world.</h1>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
           <p>
             Get acesses to all the publications <br />
             <span>for $9.99 month</span>
           </p>
+          <SubscribeButton />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl conding" />
